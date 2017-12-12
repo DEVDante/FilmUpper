245ae59a2aad7099df5b3f3494969803d1b84c44
Dante <dev_vincent@hotmail.com>
2017-12-12 20:01:39 +0100

    FrameWriter stub

---
+++ FilmUpperQt/FrameWriter.cpp
@@ -0,0 +1,93 @@
+//#include "FrameWriter.h"
+/*
+FrameWriter::FrameWriter( std::string filename, std::string format_name)
+{
+	av_register_all();
+
+	if (avformat_alloc_output_context2(&formatCTX, NULL, format_name, filename) != 0)
+		throw std::runtime_error("Couldn't allocate output format context");
+
+
+	codec = avcodec_find_encoder(codec_id);
+	if (codec == NULL) 
+		throw std::runtime_error("Unsupported codec");
+
+	codecCTX = avcodec_alloc_context3(codec);
+	if (avcodec_copy_context(codecCTX, codecCtxOriginal) != 0) 
+		throw std::runtime_error("Couldn't copy codec context");
+
+	if (avcodec_open2(codecCTX, codec, NULL)<0)
+		throw std::runtime_error("Couldn't open codec");
+
+    frame=av_frame_alloc();
+    if (frame == NULL)
+        throw std::bad_alloc::bad_alloc();
+    frameRGB=av_frame_alloc();
+    if (frameRGB == NULL)
+		throw std::bad_alloc::bad_alloc();
+
+    numBytes=avpicture_get_size(AV_PIX_FMT_RGB24, codecCTX->width, codecCTX->height);
+    frameBuffer=(uint8_t *)av_malloc(numBytes*sizeof(uint8_t));
+
+    avpicture_fill((AVPicture *)frameRGB, frameBuffer, AV_PIX_FMT_RGB24, codecCTX->width, codecCTX->height);
+
+}
+
+
+FrameReader::~FrameReader()
+{
+	avcodec_close(codecCTX);
+	avcodec_close(codecCtxOriginal);
+	avformat_close_input(&formatCTX);
+}
+
+bool FrameReader::AreFramesLeft()
+{
+    return true;
+}
+
+VideoFrame* FrameReader::ReadNextFrame()
+{
+    struct SwsContext *sws_ctx = NULL;
+    int frameFinished;
+    AVPacket packet;
+
+    sws_ctx = sws_getContext(codecCTX->width, codecCTX->height, codecCTX->pix_fmt, codecCTX->width, codecCTX->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+
+    do {
+        if (av_read_frame(formatCTX, &packet)>=0)
+            //throw std::runtime_error("Couldn't read frame data");
+            return NULL;
+        
+        if(packet.stream_index==videoStream) 
+            avcodec_decode_video2(codecCTX, frame, &frameFinished, &packet);
+    } while(!frameFinished);
+
+    sws_scale(sws_ctx, (uint8_t const * const *)frame->data, frame->linesize, 0, codecCTX->height, frameRGB->data, frameRGB->linesize);
+    
+    VideoFrame *outFrame = new VideoFrame(codecCTX->width, codecCTX->height);
+
+    for(int y = 0; y < codecCTX->height; y++) 
+        for(int x = 0; x < codecCTX->width; x++) 
+        {
+            int offset = x + y * c->linesize[0];
+
+            outFrame->Frame[x][y].setRed(frameRGB->data[0][offset + 0]);
+            outFrame->Frame[x][y].setGreen(frameRGB->data[0][offset + 1]);
+            outFrame->Frame[x][y].setBlue(frameRGB->data[0][offset + 2]);
+        }
+    
+    return outFrame;
+}
+
+FilmQualityInfo* FrameReader::GetVideoFormatInfo()
+{
+    FilmQualityInfo *nfo = new FilmQualityInfo();
+
+    nfo->FrameSizeX = codecCTX->width;
+    nfo->FrameSizeY = codecCTX->height;
+	nfo->FrameRate = (codecCTX->framerate.num) / (codecCTX->framerate.den);
+
+	return nfo;
+}
+*/
