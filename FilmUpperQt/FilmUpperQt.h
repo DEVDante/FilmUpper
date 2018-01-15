fc13637767751c97b0dcf23652badf69ffb3ea73
DEVDante <dev_vincent@hotmail.com>
2018-01-15 20:40:25 +0100

    Add depend

--- FilmUpperQt/FilmUpperQt.h
+++ FilmUpperQt/FilmUpperQt.h
@@ -22,6 +22,8 @@
 #include <QtWidgets/QFileDialog>
 #include <QtWidgets/QMessageBox>
 #include <QProcess>
+#include <QFileInfo>
+#include <QUuid>
 
 #include "FilmUpperController.h"
 #include "IFrameEnhancerHeader.h"
